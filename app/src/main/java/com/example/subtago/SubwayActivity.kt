475068a4e2c5579ac93c15117c00af07ca357a2b
STIIIIICH <djaak1435@gmail.com>
2022-05-24 10:26:46 +0900

    GPS

--- app/src/main/java/com/example/subtago/SubwayActivity.kt
+++ app/src/main/java/com/example/subtago/SubwayActivity.kt
@@ -1,13 +1,50 @@
 package com.example.subtago
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import net.daum.mf.map.api.MapView
+
 
 class SubwayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.subway_1)
+
+        // 위치 권한 요청 코드
+        val permissionCheck =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) { //포그라운드 위치 권한 확인
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
+
+        val permissionCheck2 =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) { //백그라운드 위치 권한 확인
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                0
+            )
+        }
+
+        val mapView = MapView(this)
+        val mapViewContainer = findViewById<View>(R.id.map_view) as ViewGroup
+        mapViewContainer.addView(mapView)
 
         btn_subway.setOnClickListener {
             finish() // 액티비티 종료
